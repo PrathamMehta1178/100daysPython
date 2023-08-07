cb30a2f7c65bf14b814fce80d7480b496763faf9
Pratham Mehta <134871861+PrathamMehta1178@users.noreply.github.com>
2023-08-07 13:41:27 -0500

    Add files via upload

---
+++ dayone.py
@@ -0,0 +1,5 @@
+print('Hello! Welcome to the Band Generator')
+first = input('What city did you grow up in?\n')
+second = input('What is the name of your pet?\n')
+print('Your band name is ' + first + ' ' + second)
+
---
+++ daytwopractice.py
@@ -0,0 +1,6 @@
+print('Type a two digit number.')
+num = input()
+first = int(num[0])
+second = int(num[1])
+answer = first + second
+print(answer)
